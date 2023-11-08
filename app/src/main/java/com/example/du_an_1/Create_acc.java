233d7dd66665176ab1835b5cd1a492bb8c8a3bcf
Ngocminh241 <72495719+Ngocminh241@users.noreply.github.com>
2023-11-08 23:04:48 +0700

    NgocMinh_cmt4

--- app/src/main/java/com/example/du_an_1/Create_acc.java
+++ app/src/main/java/com/example/du_an_1/Create_acc.java
@@ -2,13 +2,33 @@ package com.example.du_an_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class Create_acc extends AppCompatActivity {
-
+    Button btn_login_acc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+
+        btn_login_acc = findViewById(R.id.btn_login_acc);
+
+        btn_login_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(Create_acc.this, Login.class);
+                        startActivity(intent);
+                    }
+                }, 1200);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/du_an_1/MainActivity.java
+++ app/src/main/java/com/example/du_an_1/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.du_an_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.du_an_1.Adapter.CategoryAdapter;
+import com.example.du_an_1.Domain.CategoryDomain;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryDomains = new ArrayList<>();
+        categoryDomains.add(new CategoryDomain("Pizza", "cat_1"));
+        categoryDomains.add(new CategoryDomain("Burger", "cat_2"));
+        categoryDomains.add(new CategoryDomain("Hotdog", "cat_3"));
+        categoryDomains.add(new CategoryDomain("Drink", "cat_4"));
+        categoryDomains.add(new CategoryDomain("Donut", "cat_5"));
+
+        adapter = new CategoryAdapter(categoryDomains);
+        recyclerViewCategoryList.setAdapter(adapter);
+
     }
 }
