e853ec9bc625df8165544163a3e97e4aa6caa157
Ngocminh241 <72495719+Ngocminh241@users.noreply.github.com>
2023-11-14 13:22:14 +0700

    NgocMinh_cmt9

--- app/src/main/java/com/example/du_an_1/Create_acc.java
+++ app/src/main/java/com/example/du_an_1/Create_acc.java
@@ -1,21 +1,48 @@
 package com.example.du_an_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.du_an_1.DAO.User_DAO;
+import com.example.du_an_1.DTO.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Create_acc extends AppCompatActivity {
-    Button btn_login_acc;
+    Button btn_login_acc, btn_create_acc;
+    EditText ed_username_input_create, ed_sdt_input_create, ed_password_input_create, ed_re_password_input_create, ed_userFullname_input_create;
+    Context context;
+    String fullName, phoneNumber, userName, passWord;
+    User user;
+    User_DAO dao_user;
+    Dialog dialog;
+    List<User> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+        ed_userFullname_input_create = findViewById(R.id.ed_userFullname_input_create);
+        ed_username_input_create = findViewById(R.id.ed_username_input_create);
+        ed_sdt_input_create = findViewById(R.id.ed_sdt_input_create);
+        ed_password_input_create = findViewById(R.id.ed_password_input_create);
+        ed_re_password_input_create = findViewById(R.id.ed_re_password_input_create);
 
+        btn_create_acc = findViewById(R.id.btn_create_acc);
         btn_login_acc = findViewById(R.id.btn_login_acc);
+
+        taoDoiTuong();
 
         btn_login_acc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +57,57 @@ public class Create_acc extends AppCompatActivity {
                 }, 1200);
             }
         });
+        btn_create_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog_tb();
+            }
+        });
+    }
+    public void openDialog_tb() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Create_acc.this);
+        builder.setTitle("Save");
+        builder.setMessage("Bạn có chắc chắn muốn Save không?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                fullName = ed_userFullname_input_create.getText().toString();
+                phoneNumber = ed_sdt_input_create.getText().toString();
+                userName = ed_username_input_create.getText().toString();
+                passWord = ed_password_input_create.getText().toString();
+                user = new User();
+                user.setHoTen(fullName);
+                user.setsDT(phoneNumber);
+                user.setMaDN(userName);
+                user.setMatKhau(passWord);
+                dao_user.insert(user);
+                dialog.dismiss();
+                Intent intent = new Intent(Create_acc.this, Login.class);
+                startActivity(intent);
+                Toast.makeText(Create_acc.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    public void taoDoiTuong(){
+        dialog = new Dialog(Create_acc.this);
+        dao_user =new User_DAO(dialog.getContext());
+        list=new ArrayList<>();
+    }
+    public int validate() {
+        int check = 1;
+        if (ed_userFullname_input_create.getText().length() == 0 || ed_sdt_input_create.getText().length() == 0 || ed_username_input_create.getText().length() == 0 || ed_password_input_create.getText().length() == 0 || ed_re_password_input_create.getText().length() == 0) {
+            Toast.makeText(context, "Bạn phải nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            check = -1;
+        }
+        return check;
+
     }
 }
--- app/src/main/java/com/example/du_an_1/Login.java
+++ app/src/main/java/com/example/du_an_1/Login.java
@@ -1,18 +1,28 @@
 package com.example.du_an_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.du_an_1.DAO.User_DAO;
 
 public class Login extends AppCompatActivity {
 
     TextView tv_create_new_acc;
     Button btn_login;
+    EditText ed_username_input_login, ed_password_input_login;
+    CheckBox chkRememberPass;
+    User_DAO user_dao;
+    String strUser, strPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +31,26 @@ public class Login extends AppCompatActivity {
 
         tv_create_new_acc = findViewById(R.id.tv_create_new_acc);
         btn_login = findViewById(R.id.btn_login);
+        ed_username_input_login = findViewById(R.id.ed_username_input_login);
+        ed_password_input_login = findViewById(R.id.ed_password_input_login);
+        chkRememberPass = findViewById(R.id.chkRememberPass);
+        user_dao = new User_DAO(this);
+        //
+        SharedPreferences pref = getSharedPreferences("USER_FILE", MODE_PRIVATE);
+        String user = pref.getString("USERNAME", "");
+        String pass = pref.getString("PASSWORD", "");
+        Boolean rem = pref.getBoolean("REMEMBER", false);
+
+        ed_username_input_login.setText(user);
+        ed_password_input_login.setText(pass);
+        chkRememberPass.setChecked(rem);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(Login.this, MainActivity.class);
-                        startActivity(intent);
-                    }
-                }, 1200);
+                checkLogin();
             }
         });
-
         tv_create_new_acc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,5 +64,39 @@ public class Login extends AppCompatActivity {
                 }, 1200);
             }
         });
+    }
+
+    public void rememberUser(String u, String p, boolean status) {
+        SharedPreferences pref = getSharedPreferences("USER_FILE", MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        if (!status) {
+            // xoa trang thai luu truoc do
+            edit.clear();
+        } else {
+            edit.putString("USERNAME", u);
+            edit.putString("PASSWORD", p);
+            edit.putBoolean("REMEMBER", status);
+        }
+        // luu lai toan bo du lieu
+        edit.commit();
+    }
+
+    public void checkLogin() {
+        strUser = ed_username_input_login.getText().toString();
+        strPass = ed_password_input_login.getText().toString();
+        if (strUser.trim().isEmpty() || strPass.trim().isEmpty()) {
+            Toast.makeText(this, "Username or Password cannot be left blank", Toast.LENGTH_SHORT).show();
+        } else {
+            if (user_dao.checkLogin(strUser, strPass) > 0) {
+                Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                rememberUser(strUser, strPass, chkRememberPass.isChecked());
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("user", strUser);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
